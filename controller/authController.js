0b90ae49269482d40a0dd8cba6b529c5653bb2a8
John Garzon <john.garzon.ex@copec.cl>
2021-12-20 19:56:26 -0500

    Se crea la válidacion del jwt y se términa la app del backend

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,12 @@
-const LoginUser = require('../models/user-db');
-const bcrypt = require('bcrypt');
-const { generarJWT } = require('../helpers/jwt');
+const LoginUser = require("../models/user-db");
+const bcrypt = require("bcrypt");
+const { generarJWT } = require("../helpers/jwt");
 
-//Register Controller
+
+
+//Register Controller/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const postRegister = async(req, res) => {
     const { name, email, password } = req.body;
-
 
     try {
         //Verificar email
@@ -17,21 +18,15 @@ const postRegister = async(req, res) => {
             });
         }
 
-
         //Crear usuario con el modelo
         const dbUser = new LoginUser(req.body);
-
 
         //Hashear contraseña
         const salt = await bcrypt.genSaltSync(10);
         dbUser.password = await bcrypt.hashSync(password, salt);
 
-
-
         //Generer JWT
         const token = await generarJWT(dbUser.id, dbUser.name);
-
-
 
         //Crear usuario en la base de datos
         await dbUser.save();
@@ -42,39 +37,87 @@ const postRegister = async(req, res) => {
             name,
             email,
             password,
-            token
+            token,
         });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            ok: false,
+            msg: "Algo salio mal al registrar el usuario",
+        });
+    }
+};
+
+
+
+
+//Login controller////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const postLogin = async(req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const dbUser = await LoginUser.findOne({ email });
+        // Verificar si existe el usuario
+        if (!dbUser) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El correo no existe",
+            });
+        }
+        // Confirmar si el password hace match
+        const validarPassword = bcrypt.compareSync(password, dbUser.password);
+
+        if (!validarPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: "La contraseña no es válida",
+            });
+        }
+
+        // Generar el JTW para ingreso de usuario logeado correctamente
+        const token = await generarJWT(dbUser.id, dbUser.name);
+
+        // Respuesta del servicio
+        return res.json({
+            ok: true,
+            uid: dbUser.id,
+            name: dbUser.name,
+            token,
+        })
+
 
     } catch (err) {
         console.log(err);
         return res.status(500).json({
             ok: false,
-            msg: "Algo salio mal al registrar el usuario"
-        })
+            msg: "Hable con el administrador",
+        });
     }
-}
+};
 
 
-//Login controller
-const postLogin = async(req, res) => {
-    const { email, password } = req.body;
-    return res.json({
-        ok: true,
-        msg: "logeando un usuario ya registrado"
-    })
-}
 
 
-//Renew token
+
+//Renew token////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const getRenewToken = async(req, res) => {
+    const { uid, name } = req;
+    const token = await generarJWT(uid, name);
     return res.json({
         ok: true,
-        msg: "Validando token"
-    })
-}
+        uid,
+        name,
+        token
+    });
+};
+
+
+
+
+// Aquí se exportan cada una de la funciónes involucradas en el archivo//////////////////////////////////////////////////////////////
 
 module.exports = {
     postLogin,
     postRegister,
-    getRenewToken
-}
+    getRenewToken,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
-const {Router} = require('express');
-const {postLogin, postRegister, getRenewToken} = require("../controller/authController");
-const {check} = require("express-validator");
-const {validarCampos} = require("../middleware/validar-campos");
+const { Router } = require('express');
+const { postLogin, postRegister, getRenewToken } = require("../controller/authController");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middleware/validar-campos");
+const validarJWT = require('../helpers/validar-token');
 const router = Router();
 
 
@@ -10,8 +11,8 @@ router.post('/register', [
     check('name', 'El campo name no puede estar vacio.').not().isEmpty(),
     check('email', 'El campo email es obligatorio.').isEmail(),
     check("password", "Por favor ingrese un password al menos de  8 caracteres una mayúscula una minúscula un carácter especial y un dígito.")
-        .isLength({min: 8})
-        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/,),
+    .isLength({ min: 8 })
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/, ),
     validarCampos
 ], postRegister);
 
@@ -19,15 +20,17 @@ router.post('/register', [
 //logeando un usuario ya registrado
 router.post('/', [
     check('email', 'El campo email es obligatorio.').isEmail(),
-    check("password", "Por favor ingrese un password al menos de  8 caracteres una mayúscula una minúscula un carácter especial y un dígito. ",)
-        .isLength({min: 8})
-        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/,),
+    check("password", "Por favor ingrese un password al menos de  8 caracteres una mayúscula una minúscula un carácter especial y un dígito. ", )
+    .isLength({ min: 8 })
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/, ),
     validarCampos
 ], postLogin);
 
 
 //Validar y revalidar token
-router.get('/renew', getRenewToken);
+router.get('/renew', [
+    validarJWT
+], getRenewToken);
 
 
 module.exports = router
